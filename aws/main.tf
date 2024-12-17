bf0d52fa7a8787ba45b701111080ff4d12525c25
Jane Ho <jane.ho@jane.ho-HFV91PCF95>
2024-12-17 16:43:07 +0800

    update oidc data source

--- aws/main.tf
+++ aws/main.tf
@@ -30,7 +30,7 @@ data "aws_iam_policy_document" "stacks_role_policy" {
     effect = "Allow"
     principals {
       type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.stacks_openid_provider.arn]
+      identifiers = [data.aws_iam_openid_connect_provider.stacks_openid_provider.arn]
     }
     actions = ["sts:AssumeRoleWithWebIdentity"]
     condition {
